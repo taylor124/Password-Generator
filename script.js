e44bbd76f59fdc0b7465d9a3eeec054767a8b7d5
taylor124 <74930599+taylor124@users.noreply.github.com>
2020-12-22 07:53:13 -0500

    feature: password length

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // Assignment code here
-var uniqueCharacter = 
-["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[","]", ",", "^", "_", "`", "{", "}", "|", "~", 
-"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z", 
+var characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[","]", ",", "^", "_", "`", "{", "}", "|", "~",
+ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z",
 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y", "Z"];
+
+var numberLength = window.prompt("How many characters in your password?");
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(24);
+  var password = generatePassword(numberLength)
   
   var passwordText = document.querySelector("#password");
   passwordText.value = (password);
@@ -21,8 +24,11 @@ function writePassword() {
 function generatePassword(passLength){
   var result = "";
   for(i=0; i<passLength; i++){
-    var index = getRandomInt(uniqueCharacter.length);
-    result += uniqueCharacter[index];
+    var index = getRandomInt(characters.length);
+   
+    result += characters[index];
+    
+  
   }
   return result;
 }
